a8fdde8e991f94dddeeeae86d0eb916f90298ef0
Viktor Redecker <viktor.redecker@gmail.com>
2022-02-06 21:10:18 +0100

    add gui to chatty

---
+++ gui.py
@@ -0,0 +1,71 @@
+#Creating GUI with tkinter
+from response import Response
+from tkinter import Tk, Text, Scrollbar, Button, END, NORMAL, DISABLED, FALSE
+
+class GUI:
+    """
+    GUI class that creates chat window with TKinter
+    """
+    def __init__(self):
+        self.response = Response()
+        self.base = None
+        self.entry_box = None
+        self.chat_log = None
+        self.scrollbar = None
+        self.send_button = None
+        self._create_gui()
+    
+    def _create_gui(self):
+        """
+        Create the GUI
+        """
+        self.base = Tk()
+        self.base.title("Chatty")
+        self.base.geometry("400x500")
+        self.base.resizable(width=FALSE, height=FALSE)
+
+        #Create Chat window
+        self.chat_log = Text(self.base, bd=0, bg="white", height="8", width="50", font="Arial",)
+
+        self.chat_log.config(state=DISABLED)
+
+        #Bind scrollbar to Chat window
+        self.scrollbar = Scrollbar(self.base, command=self.chat_log.yview, cursor="heart")
+        self.chat_log['yscrollcommand'] = self.scrollbar.set
+
+        #Create Button to send message
+        self.send_button = Button(self.base, font=("Verdana",12,'bold'), text="Send", width="12", height=5,
+                            bd=0, bg="grey18", activebackground="grey73",fg='black',
+                            command= self.send)
+
+        #Create the box to enter message
+        self.entry_box = Text(self.base, bd=0, bg="white",width="29", height="5", font="Arial")
+
+
+        #Place all components on the screen
+        self.scrollbar.place(x=376,y=6, height=386)
+        self.chat_log.place(x=6,y=6, height=386, width=370)
+        self.entry_box.place(x=128, y=401, height=90, width=265)
+        self.send_button.place(x=6, y=401, height=90)
+
+    def send(self):
+        """
+        Define what happens when click on send
+        """
+        msg = self.entry_box.get("1.0",'end-1c').strip()
+        self.entry_box.delete("0.0",END)
+
+        if msg != '':
+            self.chat_log.config(state=NORMAL)
+            self.chat_log.insert(END, "You: " + msg + '\n\n')
+            self.chat_log.config(foreground="#442265", font=("Verdana", 12 ))
+
+            res = self.response.respond(msg)
+            self.chat_log.insert(END, "Bot: " + res + '\n\n')
+
+            self.chat_log.config(state=DISABLED)
+            self.chat_log.yview(END)
+
+if __name__ == '__main__':
+    gui = GUI()
+    gui.base.mainloop()
---
+++ response.py
@@ -0,0 +1,81 @@
+from keras.models import load_model
+import nltk
+import json
+import pickle
+import numpy as np
+import random
+
+class Response:
+    def __init__(self):
+        self.model = load_model('chatty.h5')
+        self.intents = json.load(open('intents.json'))
+        self.words = pickle.load(open('words.pkl', 'rb'))
+        self.classes = pickle.load(open('classes.pkl', 'rb'))
+        self.lemmatizer = nltk.WordNetLemmatizer()
+
+    def respond(self, msg):
+        ints = self._predict_class(msg)
+        res = self._get_response(ints)
+        return res
+
+    def _clean_up_sentence(self, sentence):
+        sentence_words = nltk.word_tokenize(sentence)
+        sentence_words = [self.lemmatizer.lemmatize(w.lower()) for w in sentence_words]
+        return sentence_words
+
+    def _bow(self, sentence, words, show_details=True):
+        """
+        Return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+
+        Args:
+            sentence: sentence to input
+            words: vocabulary
+            show_details (bool, optional): whether to show details. Defaults to True.
+        """
+        # tokenize the pattern
+        sentence_words = self._clean_up_sentence(sentence)
+        # bag of words - matrix of N words, vocabulary matrix
+        bag = [0]*len(words)
+        for s in sentence_words:
+            for i,w in enumerate(words):
+                if w == s:
+                    # assign 1 if current word is in the vocabulary position
+                    bag[i] = 1
+                    if show_details:
+                        print ("found in bag: %s" % w)
+        return np.array(bag)
+    
+    def _predict_class(self, sentence):
+        """
+        filter out predictions below a threshold
+
+        :param sentence: sentence to input
+        :return: predicted class list
+        """
+        # filter out predictions below a threshold
+        p = self._bow(sentence, self.words, show_details=False)
+        res = self.model.predict(np.array([p]))[0]
+        error_threshold = 0.25
+        results = [[i,r] for i,r in enumerate(res) if r>error_threshold]
+        # sort by strength of probability
+        results.sort(key=lambda x: x[1], reverse=True)
+        return_list = []
+        for r in results:
+            return_list.append({"intent": self.classes[r[0]], "probability": str(r[1])})
+        return return_list
+
+    def _get_response(self, ints):
+        """
+        Gets response
+
+        :param ints: intents
+        :param intents_json: intents file
+        :return: result
+        """
+        tag = ints[0]['intent']
+        list_of_intents = self.intents['intents']
+        for i in list_of_intents:
+            if(i['tag']== tag):
+                result = random.choice(i['responses'])
+                break
+        return result
